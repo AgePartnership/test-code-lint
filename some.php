ee77af52983fd726b64e63c5d68709b6ba9e4b29
Kieran Phillips <keztricks@gmail.com>
2018-03-01 16:50:34 +0000

    its another bad commit message!

--- some.php
+++ some.php
@@ -6,3 +6,10 @@ function abadfucntion()
 {
     echo"gregg";
 }
+
+function anotherbadfunction() {
+
+    echo "woah, that line above is empty";
+}
+
+// this line is reaaaaallllllllyyyyyyyyyy lllllooooooonnnnnnggggg maybe even too long, idk, why are you asking me, I'm a person, not a linter
